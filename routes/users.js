c2ba69c93f9a89d6a435a60fe5201c90dc9bff63
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-11-22 01:39:46 +0700

    update auth on users

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const _controllerUser = require('../controllers/users/controller_user')
+const api = require('../controllers/users/controller_user')
+const checker = require('../controllers/auth/auth')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signup', _controllerUser._signUp)
-router.post('/signin', _controllerUser._signIn)
+router.post('/signup', api._signUp)
+router.post('/signin', api._signIn)
 
-router.get('/:id_role', _controllerUser._getAllUserByRole)
-router.get('/:id', _controllerUser._deleteUser)
-router.put('/:id', _controllerUser._updateUser)
+router.get('/:id_role', api._getAllUserByRole)
+router.get('/:id', checker.authUser, api._deleteUser)
+router.put('/:id', checker.authUser, api._updateUser)
 
 module.exports = router;
