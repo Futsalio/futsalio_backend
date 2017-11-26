a9c671860ced427e7e9fbc8b390335b9c8cd8c7b
erwinwahyura <erwinwahyuramadhan@gmail.com>
2017-11-26 22:54:59 +0700

    add new routes api

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,13 @@ var router = express.Router();
 const api = require('../controllers/users/controller_user')
 const checker = require('../controllers/auth/auth')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
 router.post('/signup', api._signUp)
 router.post('/signin', api._signIn)
-
-router.get('/:id_role', api._getAllUserByRole)
-router.get('/:id', checker.authUser, api._deleteUser)
+router.get('/', api._getAllUser)
+router.get('/:id', api._getUserById)
+router.get('/role/:id_role', api._getAllUserByRole)
+router.delete('/:id', checker.authUser, api._deleteUser)
 router.put('/:id', checker.authUser, api._updateUser)
 
 module.exports = router;
